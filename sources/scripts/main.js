f051523067d1053a58257336aa060cbccdc37460
Leon <leon.denise@free.fr>
2015-09-18 23:33:58 +0200

    how to get canvas and context2d

--- sources/scripts/main.js
+++ sources/scripts/main.js
@@ -1,2 +1,8 @@
 
-console.log('Coucou')
+var canvasElement = document.getElementById("container");
+var context2d = canvasElement.getContext("2d");
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+context2d.fillStyle = "#ff9640";
+context2d.fillRect(0, 0, windowWidth, windowHeight);
