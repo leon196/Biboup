d72b6d8157d0810a09e8c28d58c59271126ba05e
Leon <leon.denise@free.fr>
2015-09-22 01:02:26 +0200

    mouse events

--- sources/scripts/main.js
+++ sources/scripts/main.js
@@ -3,12 +3,18 @@ var canvasElement = document.getElementById("container");
 var timeStarted = new Date() / 1000;
 var timeElapsed = 0;
 var draw = new Draw();
+var mouse = { x: 0, y: 0 };
 
 function init ()
 {
   // Set fullscreen
   canvasElement.width = window.innerWidth;
   canvasElement.height = window.innerHeight;
+
+  // Events
+  canvasElement.addEventListener('mousedown', onMouseDown);
+  canvasElement.addEventListener('mouseup', onMouseUp);
+  canvasElement.addEventListener('mousemove', onMouseMove);
 
   // Start main loop
   requestAnimationFrame(update);
@@ -20,11 +26,26 @@ function update ()
 
   draw.Clear();
 
-  var x = canvasElement.width / 2 + Math.sin(timeElapsed) * canvasElement.width / 4;
-  draw.Circle(x, canvasElement.height / 2, 100, "#ff403c");
+  draw.Circle(mouse.x, mouse.y, 100, "#ff403c");
 
   // Maintain loop
   requestAnimationFrame(update);
+}
+
+function onMouseDown (event)
+{
+
+}
+
+function onMouseUp (event)
+{
+
+}
+
+function onMouseMove (event)
+{
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
 }
 
 document.body.onload = init
